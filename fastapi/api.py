3a3b03e33ad82ffdc248a7085c08c70d5b211894
Suleyman2005 <saghamoghlanli22@ku.edu.tr>
2025-01-19 17:56:50 +0300

    added some endpoints

--- fastapi/api.py
+++ fastapi/api.py
@@ -1,3 +1,4 @@
+from http.client import HTTPException
 from fastapi import FastAPI
 
 import mysql.connector
@@ -5,9 +6,10 @@ import mysql.connector
 db_connection = mysql.connector.connect(
   host="localhost",
   user="root",
-  passwd="123", 
+  passwd="sultansevan2005", 
   auth_plugin='mysql_native_password'
 )
+
 print(db_connection)
 
 db_cursor = db_connection.cursor(buffered=True)
@@ -17,11 +19,38 @@ db_cursor.execute("USE fitness_tracker_db")
 app = FastAPI()
 
 
-# Define a route (endpoint) for the root path
-@app.get("/")
-def read_root():
-    query = "SELECT * FROM Gym"
+# Endpoint for register 
+@app.post("/register")
+def register(id: str, email: str, password: str, fname: str, lname: str, age: int, gender: str, weight: float, height: float):
     
+    query = """
+    INSERT INTO User (id, email, password, fname, lname, age, gender, weight, height) 
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+    """
+    db_cursor.execute(query, (id, email, password, fname, lname, age, gender, weight, height))
+    db_connection.commit()
+    return {"message": "User registered successfully"}
+
+# Endpoint for login 
+@app.get("/login")
+def login(email: str, password: str):
+    query = "SELECT * FROM User WHERE email = %s AND password = %s"
+    db_cursor.execute(query, (email, password))
+    user = db_cursor.fetchone()
+    if user:
+        return {"message": "Login successful", "user": user}
+    else:
+        return {"message": "Invalid email or password."}
     
-    db_cursor.execute(query)
-    return db_cursor.fetchall()
+# Endpoint to get user details by ID 
+@app.get("/user/{user_id}")
+def get_user(user_id: str):
+    query = "SELECT * FROM User WHERE id = %s"
+    db_cursor.execute(query, (user_id,))
+    user = db_cursor.fetchone()
+    if user:
+        return {"user": user}
+    else:
+        raise HTTPException(status_code=404, detail="User not found")
+
+
