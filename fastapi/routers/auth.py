81ee0ef941387370321cc995fb4b02eb20c2cea5
Suleyman2005 <SAGHAMOGHLANLI22@KU.EDU.TR>
2025-01-22 04:34:39 +0300

    Implemented endpoint functionality for adding memberships while registering

--- fastapi/routers/auth.py
+++ fastapi/routers/auth.py
@@ -1,3 +1,4 @@
+from datetime import date, timedelta
 from fastapi import APIRouter, HTTPException
 from db import get_db
 
@@ -8,14 +9,47 @@ router = APIRouter()
 db_connection, db_cursor = get_db()
 
 @router.post("/register")
-def register(id: str, email: str, password: str, fname: str, lname: str, age: int, gender: str, weight: float, height: float):
-    query = """
+def register(id: str, 
+             email: str, 
+             password: str, 
+             fname: str, 
+             lname: str, 
+             age: int, 
+             gender: str, 
+             weight: float, 
+             height: float, 
+             gyms: list[dict]):
+    
+    query_user = """
     INSERT INTO User (id, email, password, fname, lname, age, gender, weight, height) 
     VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
     """
-    db_cursor.execute(query, (id, email, password, fname, lname, age, gender, weight, height))
+    db_cursor.execute(query_user, (id, email, password, fname, lname, age, gender, weight, height))
+
+    query_membership = """
+    INSERT INTO Have_Membership_At (user_id, gym_name, gym_location, start_date, due_date) 
+    VALUES (%s, %s, %s, %s, %s)
+    """
+
+    for gym in gyms:
+        gym_name = gym.get("gym_name")
+        gym_location = gym.get("gym_location")
+
+        if not gym_name or not gym_location:
+            raise HTTPException(
+                status_code=400,
+                detail="Missing gym_name, gym_location, or due_date in gym membership."
+                )
+        
+         # Automatically set start_date to today and due_date to two years later
+        start_date = date.today()
+        due_date = start_date + timedelta(days=730)  # 2 years
+
+        db_cursor.execute(query_membership, (id, gym_name, gym_location, start_date, due_date))
+    
     db_connection.commit()
-    return {"message": "User registered successfully"}
+    return {"message": "User registered and gym memberships added successfully."}
+
 
 
 
